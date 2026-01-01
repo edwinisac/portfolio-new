c60ebc760f49e114a28eab94bed756e71f8e052f
edwinisac <edwinisackanjirathinkal@gmail.com>
2026-01-01 21:17:34 +0530

    adding features to intro

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -6,11 +6,11 @@ export function Contact(){
             <div className="form">
                 <h1>Contact me</h1>
                 <label htmlFor="name">Enter your Name</label>
-                <input type="text" id="name" placeholder="Fill your name" />
+                <input type="text" id="name"  />
                 <label htmlFor="mail">Enter your mail id</label>
-                <input type="text" id="mail" placeholder="example@gmail.com" />
+                <input type="text" id="mail"  />
                 <label htmlFor="number">Enter your contact Number</label>
-                <input type="text" id="number" placeholder="+91 1234567890" />
+                <input type="text" id="number" />
                 <button>send</button>
             </div>
         </div>
--- src/intro/intro.jsx
+++ src/intro/intro.jsx
@@ -3,6 +3,12 @@ import image from "../assets/profile.png";
 import "./intro.css";
 
 export function Intro() {
+  const texts = [
+    "Graduated in computer science",
+    "learning Full Stack Development",
+    "Trying to be a better human",
+  ];
+
   return (
     <div className="intro">
       <div className="details">
@@ -12,22 +18,24 @@ export function Intro() {
             <div className="yellow circle"></div>
             <div className="green circle"></div>
           </div>
+
           <div className="about">
             <p className="text-name">Hii, I'm Edwin Isac</p>
             <p className="text-fixed">I'm a Full Stack Developer</p>
-            <p className="text-changing">Graduated in computer science</p>
+            <p className="text-changing">{texts[1]}</p>
           </div>
         </div>
+
         <div className="icons">
           <ul className="icons-list">
             <li className="icon">
               <Linkedin size="30px" />
             </li>
             <li className="icon">
-              <Instagram size="30px"/>
+              <Instagram size="30px" />
             </li>
             <li className="icon">
-              <Github size="30px"/>
+              <Github size="30px" />
             </li>
             <li className="icon">
               <Mail size="30px" />
@@ -35,6 +43,7 @@ export function Intro() {
           </ul>
         </div>
       </div>
+
       <div className="image">
         <img src={image} alt="" className="profile" />
       </div>
