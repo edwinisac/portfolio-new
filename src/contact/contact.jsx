187c56a564188cf57cd50ddf6ee01729cef6aa49
edwinisac <edwinisackanjirathinkal@gmail.com>
2026-01-17 20:56:45 +0530

    front end completed with mock backend

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import "./contact.css";
+import axios from "axios";
+
 
 export function Contact(){
+    const [name,setName]=useState("");
+    const[mail,setMail]=useState("");
+    const[number,setNumber]=useState(0)    
+    const handleClick=()=>{
+        axios.post("http://localhost:5000/contacts",{
+            name:name,
+            mail:mail,
+            number:number
+        })
+    }
+    
     return(
         <div className="contact-container" id="contact">
             <div className="form">
                 <h1>Contact me</h1>
                 <label htmlFor="name">Enter your Name</label>
-                <input type="text" id="name"  />
+                <input type="text" id="name" onChange={(e)=>{
+                    setName(e.target.value);
+                }} />
                 <label htmlFor="mail">Enter your mail id</label>
-                <input type="text" id="mail"  />
+                <input type="text" id="mail" onChange={(e)=>{
+                    setMail(e.target.value);
+                }} />
                 <label htmlFor="number">Enter your contact Number</label>
-                <input type="text" id="number" />
-                <button>send</button>
+                <input type="text" id="number" onChange={(e)=>{
+                    setNumber(e.target.value)
+                }} />
+                <button onClick={handleClick}>send</button>
             </div>
         </div>
     )
